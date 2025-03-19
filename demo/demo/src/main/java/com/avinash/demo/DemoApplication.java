b4ce899b0539eb27a963536ddbaac4e127d2ceee
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-03-20 02:52:44 +0530

    [avinash] add a spring data JPA and spring BAsic

---
+++ demo/demo/src/main/java/com/avinash/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.avinash.demo;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context=SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+}
